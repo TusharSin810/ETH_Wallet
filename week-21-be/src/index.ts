d19d1d342302b8ac537f2dd464eca16d2b5fecde
TusharSin810 <tsinghal666@gmail.com>
2025-04-22 17:32:54 +0530

    PollingLoop

--- week-21-be/src/index.ts
+++ week-21-be/src/index.ts
@@ -2,7 +2,7 @@ import { id, JsonRpcProvider } from "ethers";
 
 const provider = new JsonRpcProvider("https://eth-mainnet.g.alchemy.com/v2/ch2xeAWYr9E6ShlCU3e2VI7wvh5dXXhj");
 
-async function pollBack(blockNumber: number){
+async function pollBlock(blockNumber: number){
     const logs = await provider.getLogs({
         address: "0xdac17f958d2ee523a2206206994597c13d831ec7",
         fromBlock: blockNumber,
@@ -12,4 +12,9 @@ async function pollBack(blockNumber: number){
     console.log(logs)
 }
 
-pollBack(21493826);
+async function main(){
+    const currentblock = 1;
+    while(1){
+        await pollBlock(currentblock)
+    } 
+}
